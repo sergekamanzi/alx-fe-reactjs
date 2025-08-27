b43b1f800f0790b1ff7c41ffd4b7a16c9ff16a3b
serge.kmz <127386891+sergekamanzi@users.noreply.github.com>
2025-08-27 20:12:03 +0200

    Refactor WelcomeMessage component with new content

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,11 +1,12 @@
-// src/components/WelcomeMessage.jsx
-const WelcomeMessage = () => {
-  return (
-    <div>
-      <h2>Welcome to the City Lovers App!</h2>
-      <p>Explore your favorite cities and meet amazing people.</p>
-    </div>
-  );
-};
+function WelcomeMessage() {
+        return (
+                <div>
+                            <h1>Hello everyone, I am learning React at ALX!</h1>
+                                        <p>This is a simple JSX component.</p>
+                                                    <p>I am learning about JSX!</p>
+                                                            </div>
+                                                                );
+                                                                }
 
-export default WelcomeMessage;
+                                                                export default WelcomeMessage;
+                                                                
