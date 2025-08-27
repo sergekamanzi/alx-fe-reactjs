5a1c31e3c06d55f40d491820f1ced90eefa74905
serge.kmz <127386891+sergekamanzi@users.noreply.github.com>
2025-08-27 19:55:58 +0200

    react

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+
+function App() {
+    return (
+        <div>
+              < Header />
+              < MainContent />
+              <Footer />
+            <WelcomeMessage />
+                  </div>
+                    );
+                    }
+
+                    export default App;
+
+
