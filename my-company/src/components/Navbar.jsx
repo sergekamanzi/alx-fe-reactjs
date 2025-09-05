10a7f7b5013c2eb9b4ce10461f9663253068873c
serge.kmz <127386891+sergekamanzi@users.noreply.github.com>
2025-09-06 00:32:42 +0200

    Navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,11 +2,40 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", backgroundColor: "#333", color: "#fff" }}>
-      <Link to="/" style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}>Home</Link>
-      <Link to="/about" style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}>About</Link>
-      <Link to="/services" style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}>Services</Link>
-      <Link to="/contact" style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}>Contact</Link>
+    <nav
+      style={{
+        padding: "10px",
+        backgroundColor: "#333",
+        color: "#fff",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Link
+        to="/"
+        style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}
+      >
+        Home
+      </Link>
+      <Link
+        to="/about"
+        style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}
+      >
+        About
+      </Link>
+      <Link
+        to="/services"
+        style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}
+      >
+        Services
+      </Link>
+      <Link
+        to="/contact"
+        style={{ margin: "0 10px", color: "#fff", textDecoration: "none" }}
+      >
+        Contact
+      </Link>
     </nav>
   );
 }
