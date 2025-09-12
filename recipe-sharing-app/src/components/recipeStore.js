f27f4987b71dc6f71e1d9b7ab37434a774724126
serge.kmz <127386891+sergekamanzi@users.noreply.github.com>
2025-09-12 21:29:08 +0200

    add

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,6 +5,14 @@ const useRecipeStore = create((set) => ({
   addRecipe: (newRecipe) => set((state) => ({ 
     recipes: [...state.recipes, newRecipe] 
   })),
+  updateRecipe: (id, updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map(recipe => 
+      recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+    )
+  })),
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== id)
+  })),
   setRecipes: (recipes) => set({ recipes })
 }))
 
