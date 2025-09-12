6c43229611c4cb4aabead98b4676d3987ff41583
serge.kmz <127386891+sergekamanzi@users.noreply.github.com>
2025-09-12 21:42:38 +0200

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
 import RecipeDetails from './components/RecipeDetails'
 import SearchBar from './components/SearchBar'
+import FavoritesList from './components/FavoritesList'
+import RecommendationsList from './components/RecommendationsList'
+import useRecipeStore from './components/recipeStore'
 import './App.css'
 
 function App() {
+  const generateRecommendations = useRecipeStore(state => state.generateRecommendations)
+  
+  // Generate recommendations on app load
+  useEffect(() => {
+    generateRecommendations()
+  }, [generateRecommendations])
+
   return (
     <Router>
       <div className="App">
@@ -15,6 +26,8 @@ function App() {
             <>
               <AddRecipeForm />
               <SearchBar />
+              <FavoritesList />
+              <RecommendationsList />
               <RecipeList />
             </>
           } />
